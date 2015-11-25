1b2e1bddab2e6115952593859252a2f8a3dcd546
Milena Crivella <milenaverissimo@gmail.com>
2015-11-25 11:44:48 -0200

    Game
    - Grid

---
+++ Assets/Script/Grid.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public GameObject Quadrado;
+	int linhas = 5;
+	int colunas = 5;
+
+	void Start()
+	{
+		Instanciar (linhas, colunas);
+	}
+
+	void Instanciar(int L, int C)
+	{
+		for (int i = 0; i < C; i++) 
+		{
+			for(int n = 0; n < L; n++)
+			{
+				Quadrado.transform.position = new Vector3((-10f + 1.85f*i),0, -10f - 1.85f*n);
+				Instantiate(Quadrado);
+			}
+		}
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("ooi");
+		Destroy (GameObject.FindGameObjectWithTag("Quadrado").gameObject);
+	}
+
+}
