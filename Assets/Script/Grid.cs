7baabb1b960de680a2a02c08c61e2086502c5f39
MilenaCrivella <milena.fonseca@NAVE036-VPROCOU.nave.local>
2015-11-30 12:19:27 -0200

    Grid

--- Assets/Script/Grid.cs
+++ Assets/Script/Grid.cs
@@ -4,30 +4,23 @@ using System.Collections;
 public class Grid : MonoBehaviour {
 
 	public GameObject Quadrado;
-	int linhas = 5;
-	int colunas = 5;
 
 	void Start()
 	{
-		Instanciar (linhas, colunas);
+		Instanciar (5, 5, 4 - 1.65f, 4 - 1.65f);
+		Instanciar (5, 5, -13 - 50, 4 - 1.65f);
 	}
 
-	void Instanciar(int L, int C)
+	void Instanciar(int L, int C, float point_x, float point_y)
 	{
 		for (int i = 0; i < C; i++) 
 		{
 			for(int n = 0; n < L; n++)
 			{
-				Quadrado.transform.position = new Vector3((-10f + 3f*i),0, -10f - 3f*n);
+				Quadrado.transform.position = new Vector3((point_x * i),0, point_y * n);
 				Instantiate(Quadrado);
 			}
 		}
-	}
-
-	void OnMouseDown()
-	{
-		Debug.Log ("ooi");
-		Destroy (GameObject.FindGameObjectWithTag("Quadrado").gameObject);
 	}
 
 }
