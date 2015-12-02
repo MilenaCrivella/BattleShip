71866cb3225fc8ede91d2dfaf08f6e026ca07a53
milenacrivella <milenaverissimo@gmail.com>
2015-12-02 12:17:33 -0200

    Updates! :smile:
    Grid, now in 2d

--- Assets/Script/Grid.cs
+++ Assets/Script/Grid.cs
@@ -3,24 +3,26 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
-	public GameObject Quadrado;
-
+	public GameObject Tile;
+	public GameObject canvas;
+	
 	void Start()
 	{
 		Instanciar (5, 5, 4 - 1.65f, 4 - 1.65f);
-		Instanciar (5, 5, -13 - 50, 4 - 1.65f);
+		Instanciar (5, 5, 2900, 4 - 1.65f);
 	}
-
+	
 	void Instanciar(int L, int C, float point_x, float point_y)
 	{
 		for (int i = 0; i < C; i++) 
 		{
 			for(int n = 0; n < L; n++)
 			{
-				Quadrado.transform.position = new Vector3((point_x * i),0, point_y * n);
-				Instantiate(Quadrado);
+				Tile.transform.position = new Vector2((point_x + i * 480),point_y + n * 82);
+				Tile.SetActive(true);
+				var instTile = Instantiate(Tile) as GameObject;
+				instTile.transform.SetParent(canvas.transform, false);
 			}
 		}
 	}
-
 }
---
+++ Assets/Script/MouseClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+	
+	void Start () {
+		
+	}
+
+	void Update () {
+	
+	}
+}
